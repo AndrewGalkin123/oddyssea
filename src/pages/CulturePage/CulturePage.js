d0d7c86cc5ef05df5d524004d58e6a6944ea57e7
AndrewGalkin123 <127788127788a@gmail.com>
2023-08-09 17:37:25 +0300

    first 4 themes finished on culture page

--- src/pages/CulturePage/CulturePage.js
+++ src/pages/CulturePage/CulturePage.js
@@ -7,6 +7,7 @@ import "./CulturePage.css"
 
 const CulturePage = () => {
     const { currentLanguage } = useContext(LanguageContext);
+    const culturePageTranslations = translations.culturePage[currentLanguage];
     const general = translations.general[currentLanguage];
 
     const culturalKeys = [
@@ -46,8 +47,59 @@ const CulturePage = () => {
                 marginTop: "120px"
               
             }}>
-                <h1>{general["historicalheritageFull"]}</h1>
-                <p>{general["historicalheritageFull"]}</p>
+                <h1>{general["historicalheritageFull"]}</h1><br></br>
+                <p>{culturePageTranslations["Odessa"]}</p><br></br>
+                <p>{culturePageTranslations["FoundationAndEarlyYears"]}</p><br></br>
+                <p>{culturePageTranslations["MaritimePortStatus"]}</p><br></br>
+                <p>{culturePageTranslations["SignificantBuildingsAndSquares"]}</p><br></br>
+                <p>{culturePageTranslations["LiteraryConnections"]}</p><br></br>
+                <p>{culturePageTranslations["ContemporarySignificance"]}</p><br></br>
+                <h1>{general["architectureFull"]}</h1><br></br>
+
+                <p>{culturePageTranslations["OdessaRichHistory"]}</p><br></br>
+                <p>{culturePageTranslations["OperaHouse"]}</p><br></br>
+                <p>{culturePageTranslations["ArtNouveau"]}</p><br></br>
+                <p>{culturePageTranslations["SeasidePedestrianZones"]}</p><br></br>
+                <p>{culturePageTranslations["LivingMuseum"]}</p><br></br>
+                <h1>{general["literaryHeritageFull"]}</h1><br></br>
+
+                <p>{culturePageTranslations["OdessaLiteraryHeritage"]}</p><br></br>
+                <p>{culturePageTranslations["Ilfpetrov"]}</p>
+                <p>{culturePageTranslations["TwelveChairs"]}</p>
+                <p>{culturePageTranslations["GoldenCalf"]}</p><br></br>
+                <p>{culturePageTranslations["JosephBrods"]}</p>
+                <p>{culturePageTranslations["BrodskyVerses"]}</p><br></br>
+                <p>{culturePageTranslations["OtherWriters"]}</p><br></br>
+                <p>{culturePageTranslations["InspirationAndCreativity"]}</p><br></br>
+                <p>{culturePageTranslations["LiteraryLegacy"]}</p><br></br>
+                <p>{culturePageTranslations["OdessaMuse"]}</p><br></br>
+
+                <h1>{general["musicAndFestivalsFull"]}</h1><br></br>
+                <p>{culturePageTranslations["MusicalTraditions"]}</p>
+                <p>{culturePageTranslations["MusicalTraditionsContent"]}</p><br></br>
+                <p>{culturePageTranslations["FamousMusicians"]}</p><br></br>
+                <p>{culturePageTranslations["CharlesAznavour"]}</p><br></br>
+                <p>{culturePageTranslations["LeonidUtesov"]}</p><br></br>
+                <p>{culturePageTranslations["IgorKrut"]}</p><br></br>
+                <p>{culturePageTranslations["OdessaQuartet"]}</p><br></br>
+                <p>{culturePageTranslations["MusicFestivals"]}</p>
+                <p>{culturePageTranslations["KoktebelJazz"]}</p>
+                <p>{culturePageTranslations["GoldenKeyOperetta"]}</p>
+                <p>{culturePageTranslations["OdessaJazzHolidays"]}</p><br></br>
+                <p>{culturePageTranslations["DiverseConcerts"]}</p>
+                <p>{culturePageTranslations["Conclusion"]}</p><br></br>
+
+                <h1>{general["cuisineAndGastronomyFull"]}</h1><br></br>
+                <h1>{general["languagesAndDialectsFull"]}</h1><br></br>
+                <h1>{general["folkTraditionsFull"]}</h1><br></br>
+                <h1>{general["marineFull"]}</h1><br></br>
+                <h1>{general["contemporaryArtsFull"]}</h1><br></br>
+                <h1>{general["localLifestylesFull"]}</h1><br></br>
+                <h1>{general["culturalProjectsFull"]}</h1><br></br>
+                <h1>{general["multinationalInfluenceFull"]}</h1><br></br>
+                <h1>{general["touristAttractionFull"]}</h1><br></br>
+                <h1>{general["theatersAndArtFull"]}</h1><br></br>
+                <h1>{general["museumsAndExhibitionsFull"]}</h1><br></br>
             </div>
         </div>
     );
